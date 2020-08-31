1e5d311979e4e92330799a8ec0d4eff849fd4cb2
Oleksandr Tereshchenko <toporok@gmail.com>
2020-09-01 01:15:04 +0300

    shorten_url + tests

--- url_shortener/tests/test_shortener_storage.py
+++ url_shortener/tests/test_shortener_storage.py
@@ -1,9 +1,34 @@
+from unittest.mock import patch
+
 from django.test import TestCase
+
+from url_shortener.models import ShortenedUrl
+from url_shortener.shortener import shortener_storage
 
 
 class TestShortenerStorage(TestCase):
     """
     Test shortener_storage.py module.
     """
-    # TODO
-    pass
+
+    @patch.object(shortener_storage, 'generate_url_handle', return_value='ba')
+    def test_only_one_table_row_1(self, mock_generate_url_handle):
+        """
+        Make sure test_only_one_table_row_1 and test_only_one_table_row_2 don't influence each other.
+        """
+        shortener_storage.shorten_url('someurl1')
+
+        rows = ShortenedUrl.objects.all()
+        id_list = [(row.id, row.long_url) for row in rows]
+        self.assertCountEqual(id_list, [(64, 'someurl1')])
+
+    @patch.object(shortener_storage, 'generate_url_handle', return_value='ca')
+    def test_only_one_table_row_2(self, mock_generate_url_handle):
+        """
+        Make sure test_only_one_table_row_1 and test_only_one_table_row_2 don't influence each other.
+        """
+        shortener_storage.shorten_url('someurl2')
+
+        rows = ShortenedUrl.objects.all()
+        id_list = [(row.id, row.long_url) for row in rows]
+        self.assertCountEqual(id_list, [(128, 'someurl2')])
--- url_shortener/tests/test_the_tests.py
+++ url_shortener/tests/test_the_tests.py
@@ -11,28 +11,6 @@ class TestTestCase(TestCase):
     Test the tests (make sure test interactions with the database are isolated).
     """
 
-    @patch.object(shortener_storage, 'generate_url_handle', return_value='ba')
-    def test_only_one_table_row_1(self, mock_generate_url_handle):
-        """
-        Make sure test_only_one_table_row_1 and test_only_one_table_row_2 don't influence each other.
-        """
-        shortener_storage.shorten_url('someurl1')
-
-        rows = ShortenedUrl.objects.all()
-        id_list = [(row.id, row.long_url) for row in rows]
-        self.assertCountEqual(id_list, [(64, 'someurl1')])
-
-    @patch.object(shortener_storage, 'generate_url_handle', return_value='ca')
-    def test_only_one_table_row_2(self, mock_generate_url_handle):
-        """
-        Make sure test_only_one_table_row_1 and test_only_one_table_row_2 don't influence each other.
-        """
-        shortener_storage.shorten_url('someurl2')
-
-        rows = ShortenedUrl.objects.all()
-        id_list = [(row.id, row.long_url) for row in rows]
-        self.assertCountEqual(id_list, [(128, 'someurl2')])
-
     def test_two_table_rows(self):
         ShortenedUrl.objects.create(id=444)
         ShortenedUrl.objects.create(id=333)
