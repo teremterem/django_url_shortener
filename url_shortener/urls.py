784114bbc31c313145c4e4f3271d8babd27aa45d
Oleksandr Tereshchenko <toporok@gmail.com>
2020-09-01 14:22:50 +0300

    expand_url view

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shorten_url/$', views.shorten_url, name='shorten_url'),
+    path('shorten_url/', views.shorten_url, name='shorten_url'),
     path('<str:url_handle>', views.expand_url, name='expand_url'),
 ]
