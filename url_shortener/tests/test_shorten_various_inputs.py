62d9e74fa2499fb43de33fd9dc4caef1b969c491
Oleksandr Tereshchenko <toporok@gmail.com>
2020-09-06 17:17:31 +0300

    test_shorten_various_inputs - first test finished

--- url_shortener/tests/test_shorten_various_inputs.py
+++ url_shortener/tests/test_shorten_various_inputs.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 from django.test import TestCase, Client
 
 from ..models import ShortenedUrl
+from ..shortener import shortener_storage
 
 
 class TestShortenVariousInputs(TestCase):
@@ -10,11 +13,12 @@ class TestShortenVariousInputs(TestCase):
 
     def setUp(self):
         """
-        Initialize http client
+        Initialize http client.
         """
         self.client = Client()
 
-    def test_shorten_url_with_https(self):
+    @patch.object(shortener_storage, 'generate_url_handle', return_value='abc')
+    def test_shorten_url_with_https(self, mock_generate_url_handle):
         """
         Scenario: User submits long url with hTTps:// protocol specified
             Given http client is initialized
@@ -24,6 +28,7 @@ class TestShortenVariousInputs(TestCase):
 
              Then only one record exists in shortenedurl DB table
               And long url is stored as is with no modification other than leading/training whitespaces stripped
+              And the service redirects from short url using 302 (temporary) redirect
               And the service redirects from short url to the long url that is stored
         """
         self.client.post('/shorten-url/', {
@@ -34,4 +39,6 @@ class TestShortenVariousInputs(TestCase):
         self.assertEqual(len(rows), 1)
         self.assertEqual(rows[0].long_url, 'hTTps://hello.world.com/how_are_you_doing/you_world+%20')
 
-        # TODO
+        response = self.client.get('/abc')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, 'hTTps://hello.world.com/how_are_you_doing/you_world+%20')
