3480edfa2425d28227da229d5a87d2a372ac9fdb
Oleksandr Tereshchenko <toporok@gmail.com>
2020-09-06 16:49:02 +0300

    test_shorten_various_inputs.py

--- url_shortener/tests/test_shorten_various_inputs.py
+++ url_shortener/tests/test_shorten_various_inputs.py
@@ -1,13 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from ..models import ShortenedUrl
-from ..shortener import shortener_storage
 
 
 class TestShortenVariousInputs(TestCase):
     """
     Feature: shorten_url view accepts various long url formats and processes them correctly
     """
+
+    def setUp(self):
+        """
+        Initialize http client
+        """
+        self.client = Client()
 
     def test_shorten_url_with_https(self):
         """
@@ -21,8 +26,12 @@ class TestShortenVariousInputs(TestCase):
               And long url is stored as is with no modification other than leading/training whitespaces stripped
               And the service redirects from short url to the long url that is stored
         """
-        shortener_storage.shorten_url('   hTTps://hello.world.com/how_are_you_doing/you_world%20      ')
+        self.client.post('/shorten-url/', {
+            'long_url': '   hTTps://hello.world.com/how_are_you_doing/you_world+%20      ',
+        })
 
         rows = ShortenedUrl.objects.all()
         self.assertEqual(len(rows), 1)
-        self.assertEqual(rows[0].long_url, 'hTTps://hello.world.com/how_are_you_doing/you_world%20')
+        self.assertEqual(rows[0].long_url, 'hTTps://hello.world.com/how_are_you_doing/you_world+%20')
+
+        # TODO
